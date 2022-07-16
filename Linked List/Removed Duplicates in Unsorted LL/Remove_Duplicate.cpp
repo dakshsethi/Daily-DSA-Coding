370a08d504e50eb2cc02985c990cca74976abea8
Daksh Sethi <sethidaksh02@gmail.com>
2022-07-16 22:40:17 +0530

    Day-24

--- Linked List/Removed Duplicates in Unsorted LL/Remove_Duplicate.cpp
+++ Linked List/Removed Duplicates in Unsorted LL/Remove_Duplicate.cpp
@@ -45,13 +45,6 @@ void removeDuplicate(struct Node* head) {
 
 int main() {
     struct Node* head = NULL;
-    // push(&head, 1);
-    // push(&head, 2);
-    // push(&head, 3);
-    // push(&head, 4);
-    // push(&head, 3);
-    // push(&head, 2);
-    // push(&head, 5);
     push(&head, 4);
     push(&head, 2);
     push(&head, 2);
