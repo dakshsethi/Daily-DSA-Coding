aa6694c93b6a40947ede3517b5dae8a48dbe3aaf
Daksh Sethi <sethidaksh02@gmail.com>
2022-07-25 23:42:00 +0530

    Day-27

---
+++ Trees/Sum Replacement of a BT/Sum_Replacement.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    struct TreeNode *left, *right;
+    TreeNode(): data(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): data(x), left(nullptr), right(nullptr) {}
+};
+
+void printCurrentLevel(struct TreeNode* root, int level) {
+    if(root == NULL) return ;
+    if(level == 1)
+        cout<<root->data<<" ";
+    else {
+        printCurrentLevel(root->left, level-1);
+        printCurrentLevel(root->right, level-1);
+    }
+}
+
+void levelOrder(struct TreeNode* root) {
+    int height = 3;
+    for(int i=1; i<=height; i++)
+        printCurrentLevel(root, i);
+}
+
+void sumReplace(struct TreeNode* root) {
+    if(root == NULL) return ;
+
+    sumReplace(root->left);
+    sumReplace(root->right);
+
+    if(root->left != NULL || root->right != NULL)
+        root->data = root->left->data + root->right->data;
+}
+
+int main() {
+    struct TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+
+    cout<<"Level Order Traversal of Tree: ";
+    levelOrder(root);
+
+    sumReplace(root);
+    cout<<"\nLevel Order Traversal of Tree: ";
+    levelOrder(root);
+    return 0;
+}
